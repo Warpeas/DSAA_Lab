0303a4a18b5410ccd23c95ed89d1ac0f51a58eb0
Warpeas <M4A1go@163.com>
2019-11-04 15:07:51 +0800

    Cut stick by using array, maybe its better to use ArrayList? Get WA because after addition there maybe smaller pair after addition, we need to sort the array every time. Thinking how to cut the array

--- src/Tree/CutTheStick.java
+++ src/Tree/CutTheStick.java
@@ -1,4 +1,72 @@
 package Tree;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.util.Arrays.sort;
+
 public class CutTheStick {
+    static int Huffuman(int[] a, int i, int n) {
+        int sum;
+        if (i < n - 1) {
+            sum = a[i] + a[i + 1];
+            a[++i] = sum;
+            return sum + Huffuman(a, i, n);
+        }
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt();
+            int[] a = new int[n];
+            for (int j = 0; j < n; j++) {
+                a[j] = in.nextInt();
+            }
+            out.println(Huffuman(a, 0, n));
+        }
+        out.close();
+    }
+    
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+        
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+        
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+        
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+        
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+        public char[] nextCharArray() {
+            return next().toCharArray();
+        }
+    }
 }
