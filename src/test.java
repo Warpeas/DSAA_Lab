f18264e4ca53c475582dcd570b2e888859624706
Warpeas <M4A1go@163.com>
2019-11-25 02:47:16 +0800

    Failed to find the problem myself, I have tried to generate the same data, however no error, so weired

--- src/test.java
+++ src/test.java
@@ -3,11 +3,16 @@ import java.util.Random;
 public class test {
     public static void main(String[] args) {
         Random r = new Random();
-        int n = r.nextInt(15);
-        System.out.println(1);
-        System.out.println(n);
-        for (int i = 0; i < n; i++) {
-            System.out.print(r.nextInt(10)+" ");
+        int t = r.nextInt(10);
+
+        System.out.println(t);
+        for (int i = 0; i < t; i++) {
+            int n = r.nextInt(10000);
+            System.out.println(n);
+            for (int j = 0; j < n; j++) {
+                System.out.print(r.nextInt(10000)+" ");
+            }
+            System.out.println();
         }
     }
 }
